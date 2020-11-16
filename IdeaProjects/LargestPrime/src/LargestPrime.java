388d4249e0697497bb300c0dc7568d97fda39499
Kyle Neale <kyle.neale@datadoghq.com>
2020-11-16 09:43:33 -0500

    DiagonalStar Java implementation

--- IdeaProjects/LargestPrime/src/LargestPrime.java
+++ IdeaProjects/LargestPrime/src/LargestPrime.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class LargestPrime {
+public class LargestPrime {
+
+    public static int getLargestPrime(int number) {
+        if(number < 2){
+            return -1;
+        }
+
+        int i;
+
+        for (i = 2; i <= number; i++) {
+            if (number % i == 0) {
+                number /= i;
+                i--;
+            }
+        }
+        return i;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getLargestPrime(10));
+    }
 }
