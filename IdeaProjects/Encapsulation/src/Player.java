a21b1059737d2e0e157976e7fc45f02c09ffa380
Kyle Neale <kyle.neale@datadoghq.com>
2020-11-21 08:12:45 -0500

    Java Encapsulation

---
+++ IdeaProjects/Encapsulation/src/Player.java
@@ -0,0 +1,12 @@
+public class Player {
+    public String name;
+    public int health;
+    public String weapon;
+
+    public void loseHealth(int damage) {
+        this.health = this.health - damage;
+        if (this.health <= 0) {
+            System.out.println("Player got knocked out.");
+        }
+    }
+}
